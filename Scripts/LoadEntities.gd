9d8b9f7a5c82c0354d1d5df37acfd461e1734d27
AdventureT <le@nepelius.at>
2023-05-26 17:43:19 +0200

    Some changes with loading txts

--- Scripts/LoadEntities.gd
+++ Scripts/LoadEntities.gd
@@ -1,18 +1,39 @@
 extends Node
 
-func LoadTxt(filename):
-	var result = {}
-	var f = FileAccess.open(filename, FileAccess.READ)
-	
-	while f.get_position() < f.get_length():
-		var str = f.get_line()
-		RegEx.
-		print(str)
-		
+var re = RegEx.new()
+var regex = "\\b[\\w\\d]+\\b"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	LoadTxt("C:\\Users\\Leon\\Desktop\\The Good the Egg and the Ugly\\Game\\data\\output\\data\\loading\\sprites.txt")
+	var file_path = "C:\\Users\\Leon\\Desktop\\The Good the Egg and the Ugly\\Game\\data\\output\\data\\loading\\sprites.txt"
+	var string_data = loadStringFromFile(file_path)
+
+	var result = splitLinesAndWordsDigits(string_data)
+	print(result)
+
+func loadStringFromFile(file_path: String) -> String:
+	var string_data = ""
+
+	var file = FileAccess.open(file_path, FileAccess.READ)
+	string_data = file.get_as_text()
+
+	return string_data
+
+func splitLinesAndWordsDigits(string_data: String) -> Array:
+	var lines = string_data.split("\n")
+	var result = []
+
+	for line in lines:
+		line = line.replace("\r", "")  # Remove carriage return characters
+		var words_digits = line.split("\t",false)
+		var matches = []
+
+		#for i in range(words_digits.size()):
+			#matches.append(words_digits.get(i).get_string(0))
+
+		result.append(matches)
+
+	return result
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
