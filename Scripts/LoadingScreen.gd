5eaa0fdf7316a3b6a8a05574fcd1f44d2e24b9eb
AdventureT <le@nepelius.at>
2023-05-26 22:18:18 +0200

    Some minor tweaks

--- Scripts/LoadingScreen.gd
+++ Scripts/LoadingScreen.gd
@@ -6,23 +6,25 @@ extends Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var data = loading_sprites_data.get_data()
-	var file_path = "C:\\Users\\Leon\\Desktop\\The Good the Egg and the Ugly\\Game\\data\\output\\data\\loading\\sprites\\{0}.jpg".format({0:data[0][0]})
-	var img = Image.new()
-	img.load(file_path)
-	var texture = ImageTexture.create_from_image(img)
-	
-	$".".texture = texture
-	$".".centered = false
-	var viewportWidth = get_viewport().size.x
-	var viewportHeight = get_viewport().size.y
+	for data1 in data:
+		var file_path = "C:\\Users\\Leon\\Desktop\\The Good the Egg and the Ugly\\Game\\data\\output\\data\\loading\\sprites\\{0}.jpg".format({0:data1[0]})
+		var img = Image.new()
+		img.load(file_path)
+		var texture = ImageTexture.create_from_image(img)
+		if data1[1] != "static":
+				push_error("Not supporting RLE Sprites")
+		if texture != null:
+			$".".texture = texture
+			$".".centered = false
+			var viewportWidth = get_viewport().size.x
+			var viewportHeight = get_viewport().size.y
 
-	var scaleX = viewportWidth / $".".texture.get_size().x
-	var scaleY = viewportHeight / $".".texture.get_size().y
+			var scaleX = viewportWidth / $".".texture.get_size().x
+			var scaleY = viewportHeight / $".".texture.get_size().y
 
-	$".".set_scale(Vector2(scaleX, scaleY))
-	
-	if data[0][1] != "static":
-		push_error("Not supporting animated loading screens rn")
+			$".".set_scale(Vector2(scaleX, scaleY))
+			
+			
 	
 	
 
